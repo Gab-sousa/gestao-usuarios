69d4b9e791cb05d199b60d8131dd7ed2001f08e9
Gabriel <gab.sousa0208@gmail.com>
2024-10-29 11:07:27 -0300

    project base for user management

---
+++ database/cliente.py
@@ -0,0 +1,27 @@
+CLIENTES = [
+  {
+    "id": 1,
+    "nome": "Alice Santos",
+    "email": "alice.santos@example.com"
+  },
+  {
+    "id": 2,
+    "nome": "Bruno Lima",
+    "email": "bruno.lima@example.com"
+  },
+  {
+    "id": 3,
+    "nome": "Carla Souza",
+    "email": "carla.souza@example.com"
+  },
+  {
+    "id": 4,
+    "nome": "Diego Ferreira",
+    "email": "diego.ferreira@example.com"
+  },
+  {
+    "id": 5,
+    "nome": "Eduarda Oliveira",
+    "email": "eduarda.oliveira@example.com"
+  }
+]
